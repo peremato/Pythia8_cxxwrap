cba81d9dcbe1f8fa43b51590aa049277bd84fed1
Pere Mato <pere.mato@cern.ch>
2024-11-05 10:19:47 +0100

    Re-generated wrappers with latest wrapit and CxxWrap

--- src/jl/Pythia8-generated.jl
+++ src/jl/Pythia8-generated.jl
@@ -5,7 +5,8 @@ import Base.getindex
 import Base.setindex!
 
 using CxxWrap
-@wrapmodule(()->"libjlPythia8")
+import Libdl
+@wrapmodule(()->"$(@__DIR__)/../deps/libjlPythia8." * Libdl.dlext)
 
 function __init__()
     @initcxx
